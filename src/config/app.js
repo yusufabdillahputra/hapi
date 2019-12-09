c30ee177db3c913b1242f897e07a349305e17aab
yusufabdillahputra <yusufabdillahputra@gmail.com>
2019-12-09 09:53:54 +0700

    Cors

--- src/config/app.js
+++ src/config/app.js
@@ -12,7 +12,7 @@ const whitelist = [
   'http://54.144.101.230:80'
 ]
 const corsOptionsDelegate = function (req, callback) {
-  let corsOptions;
+  let corsOptions
   if (whitelist.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
   } else {
@@ -24,18 +24,15 @@ const corsOptionsDelegate = function (req, callback) {
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`)
 })
-app.use(
-  bodyParser.json(),
-  bodyParser.urlencoded({ extended: true }),
-  xssFilter({
-    setOnOldIE: true,
-    reportUri: '/report-xss-violation'
-  }),
-  cors({
-    origin: '*'
-  }),
-  express.static('storage/image'),
-  routes
-)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(xssFilter({
+  setOnOldIE: true,
+  reportUri: '/report-xss-violation'
+}))
+app.use(cors({
+  origin: false
+}))
+app.use(express.static('storage/image'))
+app.use(routes)
 
 module.exports = app
