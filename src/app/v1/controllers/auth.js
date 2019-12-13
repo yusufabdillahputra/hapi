2b0588d47c62ddc490b78470615c3ec6e37e680a
yusufabdillahputra <yusufabdillahputra@gmail.com>
2019-12-13 11:50:28 +0700

    Auth

--- src/app/v1/controllers/auth.js
+++ src/app/v1/controllers/auth.js
@@ -38,7 +38,8 @@ module.exports = {
       if (countAccount === 0 || countAccount === null) {
         response(res, 200, 401, {
           message: 'Account not register yet',
-          account: account
+          account: account,
+          body: req.body
         })
       }
     } catch (error) {
