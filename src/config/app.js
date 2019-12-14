b4205e42a3400948ae7abd0c3e834c6a3b66f5ac
yusufabdillahputra <yusufabdillahputra@gmail.com>
2019-12-15 03:09:20 +0700

    Config app

--- src/config/app.js
+++ src/config/app.js
@@ -9,30 +9,30 @@ const routes = require(`../app/v${API_VERSION}/routes`)
 const cors = require('cors')
 const morgan = require('morgan')
 
-const whitelist = [
-  'http://54.144.101.230:80'
-]
-const corsOptionsDelegate = function (req, callback) {
-  let corsOptions
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// const whitelist = [
+//   'http://54.144.101.230:80'
+// ]
+// const corsOptionsDelegate = function (req, callback) {
+//   let corsOptions
+//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`)
 })
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 // app.use(xssFilter({
 //   setOnOldIE: true,
 //   reportUri: '/report-xss-violation'
 // }))
-app.use(cors())
+//app.use(cors())
 app.use(express.static('storage/image'))
 app.use(routes)
 
